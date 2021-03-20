fc71bde10ceead2454e97aa3d78e4f1306a03098
raquel <raquel.massarope@outlook.com>
2021-03-20 20:12:24 -0300

    Criação das APIS de Produto

---
+++ Backend/PontoVirgulaApi/PontoVirgulaApi/Controllers/ModelViews/ProdutoModelView.cs
@@ -0,0 +1,15 @@
+﻿namespace PontoVirgulaApi.Models
+{
+    public class ProdutoModelView
+    {
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int Estoque { get; set; }
+
+        public string LinkImg { get; set; }
+    }
+}
